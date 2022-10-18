91cefb2d5618594a14c647504900720f2a13eea8
Helloysa <helloysapires.contato@gmail.com>
2022-10-18 16:57:52 -0300

    Create insertion-sort.c

---
+++ algoritmos-de-ordenação/insertion-sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void  insertionSort(int * array, int tamanhoArray){
+    
+    int copia, indice;
+    
+    for(int i = 1; i < tamanhoArray; i++){
+        copia = array[i]; 1
+        indice = i;  2
+        
+        while(indice > 0 && array[indice-1] > copia){
+            array[indice] = array[indice-1];
+            indice--; 
+        }
+        
+        array[indice] = copia;
+    }
+}
+
+void imprimeArray(int * array, int tamanhoArray){
+    
+    for(int i=0; i<tamanhoArray; i++){
+        printf("%d ",  array[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int array[] = {5, 7, 1, 3, 9};
+    
+    
+    printf("Vetor original: ");
+    imprimeArray(array, 5);
+    
+    printf("Vetor ordenado: ");
+    insertionSort(array, 5);
+    imprimeArray(array, 5);
+    
+    return 0;
+}
